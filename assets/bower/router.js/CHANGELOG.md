82735a6cdc8cd5d93747881c582125756c81f5cb
Thoriq Firdaus <tfirdaus@outlook.com>
2016-04-13 15:15:25 +0700

    initial commit

---
+++ assets/bower/router.js/CHANGELOG.md
@@ -0,0 +1,66 @@
+Changelog
+=========
+### 1.0.5
+
+- Updated dev-dependency
+- Fixed bug building documentation
+- Tests are done on latest version of node to behave like    
+  in development environment
+- Update readme with information on Internet Explorer 8 compatibility
+- Updated documentation
+
+### v1.0.4
+
+- Improved documentation
+- Removed unused dev dependencies
+- Updated old dependencies
+- Fixed an error on strict javascript
+- Fixed some typos on documentation
+
+### v1.0.1
+- minor bugfixes
+
+### v1.0.0
+- Added documentation
+- New tests added
+- Added `req.hasNext` properties to match presence of more mathing routes
+
+
+### v 0.8.4
+- Enabled removed tests
+
+### v 0.8.3
+- Grunt configured
+- JSHint of code
+- Karma Grunt integration
+- Removed unused warning from tests
+- Test completed
+
+### v 0.8.2
+- Added some tests
+
+### v 0.8.0
+- Fixed return value for all functions
+- Added `destroy` method to clean all listeners
+
+### v 0.7.1
+- Correct return value in "run" method
+- Introduced a test suite
+
+### v 0.7.0
+- Request object has `get` method to retrieve parameters wherever they are passed
+- Bugfix: Splats do not contain query anymore in some cases
+
+
+### v 0.6.3
+- Added support for options. `ignorecase` option, default true
+
+### v 0.6.2
+- Added map file
+- Bugfix: Query string do not appear in regular params value
+- Internal : Added closure compiler support
+
+### v 0.6.1
+- Added alias name for method `addRoute`. Now can be called `add`, `route`, `get`.
+
+Old changelog missing
---
+++ assets/bower/router.js/karma.conf.js
@@ -0,0 +1,78 @@
+// Karma configuration
+// Generated on Sat Mar 22 2014 14:03:49 GMT+0100 (CET)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai'],
+
+    client: {
+        mocha: {
+            ui: 'bdd'
+            //,require: 'should'
+        }
+    },
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      { pattern : 'node_modules/mocha/mocha.js', included: false },
+      { pattern :'src/router.js',served:true,included:true,watched:true },
+      //'tests/*.js',
+      'tests/**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      '*.min.js'
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['mocha'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'/*,'Firefox','Chrome'*/],
+    //browsers: ['Firefox'/*,'Firefox','Chrome'*/],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true
+  });
+};
